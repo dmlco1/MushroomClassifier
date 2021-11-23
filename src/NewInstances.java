fa7ea9c2ad466dd366ccf3c2051066ac8b25c363
dmlco1 <dmlco1@iscte-iul.pt>
2021-11-23 10:51:02 +0000

    newInstances class updated.

--- src/NewInstances.java
+++ src/NewInstances.java
@@ -1,9 +1,10 @@
+
 import weka.core.DenseInstance;
 import weka.core.Instances;
 import weka.core.converters.ConverterUtils.DataSource;
 
 public class NewInstances {
-    Instances database = null;
+    Instances database;
 
     public NewInstances(Instances database) {
         database.clear();
@@ -15,7 +16,11 @@ public class NewInstances {
         if (nrAttr == attributes.length) {
             double[] instancesValue = new double[nrAttr];
             for (int i = 0; i < nrAttr; i++) {
-                instancesValue[i] = database.attribute(i).indexOfValue(attributes[i]);
+                if (database.attribute(i).indexOfValue(attributes[i]) >= 0) {
+                    instancesValue[i] = database.attribute(i).indexOfValue(attributes[i]);
+                } else {
+                    instancesValue[i] = Double.parseDouble(attributes[i]);
+                }
             }
             database.add(new DenseInstance(1.0, instancesValue));
         } else {
@@ -27,25 +32,5 @@ public class NewInstances {
         return database;
     }
 
-    public static void main(String[] args) {
-        DataSource source;
-        try {
-            source = new DataSource("pratica10.arff");
-            Instances dataset = source.getDataSet();
-            dataset.setClassIndex(dataset.numAttributes() - 1);
-
-            NewInstances ni = new NewInstances(dataset);
-
-            String[] values2 = {"TRUE","rainy", "unavailable", "go_to_beach"};
-
-            ni.addInstance(values2);
-            System.out.println(ni.getDataset());
-
-        } catch (Exception e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        }
-
-    }
 
 }
